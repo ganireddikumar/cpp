478f68ef208fec2382b9317e9466ce8c27ca7ae2
Ganireddi Kumar <seoinvane@gmail.com>
2023-10-30 22:42:09 +0530

    Update countrotation.cpp

--- countrotation.cpp
+++ countrotation.cpp
@@ -2,28 +2,17 @@
 using namespace std;
 
 int countRotations(int arr[], int n) {
-    int low = 0;
-    int high = n - 1;
-
-    while (low <= high) {
-        if (arr[low] <= arr[high]) {
-            return low; // No rotations, the array is not rotated
-        }
-
+    int low = 0, high = n - 1;
+    while (low < high) {
         int mid = low + (high - low) / 2;
-        int next = (mid + 1) % n;
-        int prev = (mid - 1 + n) % n;
 
-        if (arr[mid] <= arr[next] && arr[mid] <= arr[prev]) {
-            return mid; // Mid is the minimum element, and it corresponds to the number of rotations.
-        } else if (arr[low] <= arr[mid]) {
-            low = mid + 1; // The minimum element is in the right half
+        if (arr[mid] > arr[high]) {
+            low = mid + 1;
         } else {
-            high = mid - 1; // The minimum element is in the left half
+            high = mid;
         }
     }
-
-    return -1; // Invalid input, or the array is not sorted.
+    return low;
 }
 
 int main() {
@@ -37,11 +26,7 @@ int main() {
 
     int rotations = countRotations(arr, n);
 
-    if (rotations != -1) {
-        cout << "Number of rotations: " << rotations << endl;
-    } else {
-        cout << "Invalid input or the array is not rotated." << endl;
-    }
+    cout << "Number of rotations: " << rotations << endl;
 
     return 0;
 }
